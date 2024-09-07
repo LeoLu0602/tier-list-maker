d3512fa025231a7a65a78a2784016c9a7f6b09a6
Leo Lu <lyuholu@gmail.com>
2024-09-06 21:16:55 -0400

    feat: only show sign-out button if the user is viewing his/her own profile

--- app/user/[userId]/page.tsx
+++ app/user/[userId]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Dispatch, useEffect } from 'react';
+import { Dispatch, useEffect, useState } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import { useAuth, useAuthDispatch } from '@/app/contexts/AuthContext';
@@ -8,6 +8,7 @@ import { getUserInfo, signOut } from '@/app/lib/utils';
 import { ActionType, AuthType } from '@/types';
 
 export default function Page() {
+  const [userInfo, setUserInfo] = useState<AuthType | null>(null);
   const auth: AuthType | null = useAuth();
   const dispatch: Dispatch<ActionType> = useAuthDispatch();
   const router: AppRouterInstance = useRouter();
@@ -16,9 +17,7 @@ export default function Page() {
 
   useEffect(() => {
     async function setUp() {
-      const userInfo = await getUserInfo(userId);
-
-      console.log(userInfo);
+      setUserInfo(await getUserInfo(userId));
     }
 
     setUp();
@@ -27,19 +26,24 @@ export default function Page() {
   return (
     <>
       <section className="flex gap-4 h-20 items-center">
-        <img className="w-20 h-20 rounded-full" src={auth?.avatarUrl ?? ''} />
+        <img
+          className="w-20 h-20 rounded-full"
+          src={userInfo?.avatarUrl ?? ''}
+        />
         <div className="flex flex-col items-start justify-between h-full">
-          <h2 className="font-bold text-4xl">{auth?.name ?? ''}</h2>
-          <button
-            className="text-base bg-rose-500 rounded-md h-8 w-20"
-            onClick={async () => {
-              await signOut();
-              dispatch({ type: 'sign-out' });
-              router.push('/');
-            }}
-          >
-            Sign out
-          </button>
+          <h2 className="font-bold text-4xl">{userInfo?.name ?? ''}</h2>
+          {auth && userInfo && auth.userId === userInfo.userId && (
+            <button
+              className="text-base bg-rose-500 rounded-md h-8 w-20"
+              onClick={async () => {
+                await signOut();
+                dispatch({ type: 'sign-out' });
+                router.push('/');
+              }}
+            >
+              Sign out
+            </button>
+          )}
         </div>
       </section>
     </>
