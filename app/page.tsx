0dc81b5dddd5de7394be8dd868a7109d7328fb9e
Leo Lu <lyuholu@gmail.com>
2024-11-05 19:46:43 -0500

    style: change hover effect on template

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
       <h1 className="font-bold text-4xl mb-4">
         Create a Tier List for Anything
       </h1>
-      <h2 className="mb-4 text-[#bababa]">
+      <h2 className="mb-8 text-[#bababa]">
         A tier list is a ranking system that allows you to rank anything in
         tiers from the best to worst. Using a tier list allows you to group
         similar ranked items together and it’s quick and easy to create a tier
