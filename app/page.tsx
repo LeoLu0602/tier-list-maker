e66a7bfbf8b04781ab9a1aa4a3bb2eb37b35db0a
Leo Lu <lyuholu@gmail.com>
2024-11-26 19:38:09 -0500

    feat: add link to TierMaker

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,19 @@ export default async function Page() {
       <h1 className='mb-4 animate-fadeIn text-4xl font-bold'>
         Create a Tier List for Anything
       </h1>
-      <h2 className='mb-8 animate-fadeIn text-base leading-8 text-[#bababa]'>
+      <p className='mb-8 animate-fadeIn text-base text-[#bababa]'>
         A tier list is a ranking system that allows you to rank anything in
         tiers from the best to worst. Using a tier list allows you to group
         similar ranked items together and it’s quick and easy to create a tier
-        list.
-      </h2>
+        list. Tier List Maker is inspired by&nbsp;
+        <a
+          className='text-sky-500'
+          href='https://tiermaker.com/'
+          target='_blank'
+        >
+          TierMaker
+        </a>
+      </p>
       <h3 className='mb-4 text-xl font-bold text-white'>Featured Templates</h3>
       <section className='flex gap-2 overflow-auto'>
         {templates.map(({ id, poster, title }) => (
