4f6ca498189eeb981eb451458fe6815da396dc20
Leo Lu <lyuholu@gmail.com>
2024-11-26 19:19:51 -0500

    style: sort tailwind classes automatically

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,17 @@ export default async function Page() {
 
   return (
     <>
-      <h1 className="font-bold text-4xl mb-4 animate-fadeIn">
+      <h1 className='mb-4 animate-fadeIn text-4xl font-bold'>
         Create a Tier List for Anything
       </h1>
-      <h2 className="mb-8 text-[#bababa] animate-fadeIn text-base leading-8">
+      <h2 className='mb-8 animate-fadeIn text-base leading-8 text-[#bababa]'>
         A tier list is a ranking system that allows you to rank anything in
         tiers from the best to worst. Using a tier list allows you to group
         similar ranked items together and it’s quick and easy to create a tier
         list.
       </h2>
-      <h3 className="text-white font-bold mb-4 text-xl">Featured Templates</h3>
-      <section className="flex gap-2 overflow-auto">
+      <h3 className='mb-4 text-xl font-bold text-white'>Featured Templates</h3>
+      <section className='flex gap-2 overflow-auto'>
         {templates.map(({ id, poster, title }) => (
           <Template
             key={id}
