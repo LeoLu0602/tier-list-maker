f05be4e5382f5788ef5fbcf7d9dbe5e63a36840a
Leo Lu <lyuholu@gmail.com>
2024-11-28 18:37:53 -0500

    style: make user page responsive

--- components/Preview.tsx
+++ components/Preview.tsx
@@ -8,9 +8,9 @@ export default function Template({
     title: string;
 }) {
     return (
-        <section className='w-1/2 p-1'>
+        <section className='lg:w-1/2 w-full p-1'>
             <a href={link}>
-                <img className='h-60 w-full object-left-top' src={poster} />
+                <img className='w-full h-72 object-left-top object-cover' src={poster} />
                 <div className='w-full overflow-hidden text-ellipsis whitespace-nowrap bg-black p-4'>
                     {title}
                 </div>
